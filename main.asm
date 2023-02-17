197e110c84c52769e9ec9a67337cd1ad8a8ea118
Martin Hansson <martinhson@gmail.com>
2023-02-17 20:21:53 +0100

    Formatting, removed build files.

--- main.asm
+++ main.asm
@@ -58,23 +58,24 @@ screen_top: defb    0   ; WPMEMx
 
 Stack_Top:		EQU 0xFFF0
 			LD SP, Stack_Top
-            
+
 main:
+jqqw
 	DI
 	LD SP, Stack_Top
 	LD A,38h
 	CALL Clear_Screen
-	LD IX,Text_Scores
+	LD IX, Text_Scores
 	CALL Print_String
 	CALL Initialise_Sprites
-	LD HL,Interrupt
-	LD IX,0xFFF0
-	LD (IX+04h),0xC3         		   ; Opcode for JP
-	LD (IX+05h),L
-	LD (IX+06h),H
-	LD (IX+0Fh),0x18        		    ; Opcode for JR; this will do JR to FFF4h
-	LD A,0x39
-	LD I,A
+	LD HL, Interrupt
+	LD IX, 0xFFF0
+	LD (IX + 04h), 0xC3	   ; Opcode for JP
+	LD (IX + 05h), L
+	LD (IX + 06h), H
+	LD (IX + 0Fh), 0x18	    ; Opcode for JR; this will do JR to FFF4h
+	LD A, 0x39
+	LD I, A
 	IM 2
 	EI
 
