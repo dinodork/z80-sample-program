bed34a1e5bd192dd7cf754a56f7cd2b2958e90a1
Martin Hansson <martinhson@gmail.com>
2023-07-11 23:25:42 +0300

    Using the font.

--- scripts/generate_asm.sh
+++ scripts/generate_asm.sh
@@ -1,5 +1,7 @@
 BUILD_DIR=$1
 DIR=$2
+
 mkdir -p ${BUILD_DIR}/${DIR}
 scripts/export_to_bin.py graphics/bubble2x2.udg -o build/graphics/bubble2x2.asm
 scripts/export_to_bin.py graphics/robot.udg -o build/graphics/robot.asm
+scripts/export_to_bin.py -W1 graphics/npm_large.fnt -o build/graphics/npm_large.asm
